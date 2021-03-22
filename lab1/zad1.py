f04363e6f8da0172db2a24cf2a60c75f304f55e1
JBratulic <jelena.bratulic@gmail.com>
2021-03-22 20:58:39 +0100

    Lab1a: Finished 1a.

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -4,32 +4,11 @@ import random
 import queue
 import time
 import sys
-import numpy as np
 
-"""
-Stari most je uski most i stoga postavlja ograničenja na promet. 
-Na njemu istovremeno smije biti najviše 3 automobila koji voze u istom smjeru. 
-Simulirati automobile procesom Auto  koji obavlja niže navedene radnje. Napisati program koji stvara N  automobila, 
-gdje je N proizvoljan broj između 5 i 100 koji se određuje prilikom pokretanja programa te svakom automobilu dodjeljuje 
-registarsku oznaku. Smjer se automobilu određuje nasumično.
 
-Proces semafor određuje koji automobili će prijeći most, a početni smjer prijelaza se određuje nasumično te se zatim 
-izmjenjuju. Prijelazak mosta se omogućuje kada se zabilježi 3 zahtjeva za prijelaz u trenutnom smjeru ili prođe X 
-milisekundi, gdje je X slučajan broj između 500 i 1000. 
-Prijelaz mosta traje Y milisekundi gdje je Y broj između 1000 i 3000.
-
-Procesi međusobno komuniciraju uz pomoć reda poruka koristeći raspodijeljeni centralizirani protokol, 
-gdje je proces Semafor odgovoran za međusobno isključivanje.
-
-Proces Auto(registarska_oznaka, smjer) {
-   // smjer = 0 ili 1
-   // registarska oznaka je redni broj automobila u sustavu
-   spavaj Z milisekundi; // Z je slučajan broj između 100 i 2000
-   pošalji zahtjev za prijelaz mosta i ispiši("Automobil registarska_oznaka čeka na prelazak preko mosta");
-   po primitku poruke "Prijeđi" ispiši("Automobil registarska_oznaka se popeo na most");
-   po primitku poruke "Prešao" ispiši("Automobil registarska_oznaka je prešao most.");
-}
-"""
+ZELIM_NA_MOST = 0
+ODI_NA_MOST = 1
+SIDI_S_MOSTA = 2
 
 
 class Car:
@@ -39,56 +18,51 @@ class Car:
         self.times_to_cross = times_to_cross
         self.process = None
 
-    def make_process(self, q):
-        self.process = Process(target=self.run, args=(q, ""))
+    def make_process(self, q1, q2):
+        self.process = Process(target=self.run, args=(q1, q2))
         self.process.start()
 
-    def run(self, q, a):
+    def run(self, q1, q2):
         for i in range(self.times_to_cross):
             # Spavaj neko vrijeme između 100 i 2000 milisekundi
             time.sleep(random.uniform(0.1, 2))
 
             # Zatraži prelazak preko mosta
-            q.put(["Ja bih na most", self.reg_plate, self.direction])
+            q1.put([ZELIM_NA_MOST, self.reg_plate, self.direction])
             sys.stdout.write("Automobil {} čeka na prelazak preko mosta u smjeru {}.\n".format(self.reg_plate,
                                                                                                self.direction))
             sys.stdout.flush()
 
             # Čitaj poruke dok ne dobiš poruku Popni se i svoju registracijsku oznaku
             while True:
-                message, reg_plate, direction = q.get()
-                if message != "Popni se":
-                    q.put([message, reg_plate, direction])
+                message, reg_plate, direction = q2.get()
+                if message != ODI_NA_MOST:
+                    q2.put([message, reg_plate, direction])
                 else:
                     if reg_plate == self.reg_plate:
                         break
                     else:
-                        q.put([message, reg_plate, direction])
+                        q2.put([message, reg_plate, direction])
 
             sys.stdout.write("Automobil {} se popeo na most.\n".format(self.reg_plate))
             sys.stdout.flush()
 
             # Čitaj poruke dok ne dobiš poruku Siđi i svoju registracijsku oznaku
             while True:
-                message, reg_plate, direction = q.get()
-                if message != "Sidi dole":
-                    q.put([message, reg_plate, direction])
+                message, reg_plate, direction = q2.get()
+                if message != SIDI_S_MOSTA:
+                    q2.put([message, reg_plate, direction])
                 else:
                     if reg_plate == self.reg_plate:
                         break
                     else:
-                        q.put([message, reg_plate, direction])
+                        q2.put([message, reg_plate, direction])
 
             sys.stdout.write("Automobil {} je prešao most.\n".format(self.reg_plate))
             sys.stdout.flush()
 
             # Okreni se polukružno i započni opet proces prelaska preko mosta
             self.direction = (self.direction + 1) % 2
-        """
-        sys.stdout.write("Automobil {} je prešao most {} puta i krenuo doma.\n".format(self.reg_plate,
-                                                                                     self.times_to_cross))
-        sys.stdout.flush()
-        """
 
 
 class Bridge:
@@ -96,16 +70,18 @@ class Bridge:
         self.n_process = random.randint(n_process_min, n_process_max)
         self.times_to_cross = times_to_cross
         self.max_cars = n_max_cars
-        self.q = Queue()
+        self.q1 = Queue()
+        self.q2 = Queue()
         self.my_direction = random.randint(0, 1)
         self.cars = []
 
     def run(self):
+        # Stvori aute i pokreni njihove procese
         for idx in range(self.n_process):
             self.cars.append(Car(idx, self.times_to_cross))
 
         for car in self.cars:
-            car.make_process(self.q)
+            car.make_process(self.q1, self.q2)
 
         active_cars = self.n_process
 
@@ -114,31 +90,31 @@ class Bridge:
 
         ids_0 = queue.Queue()
         ids_1 = queue.Queue()
+        print("Stvoreno {} auta".format(self.n_process))
 
+        # Ponavljaj izmjenu semafora dok svi auti ne produ preko mosta
         while active_cars > 0:
-            print("Aktivnih auti ima {}, a moj smjer je {}".format(active_cars, self.my_direction))
-
             start_time = time.perf_counter()
-            print("Velicine | q={}, c0={}, {}, c1={}, {}".format(self.q.qsize(), dir_0_counter, ids_0.qsize(),
-                                                                 dir_1_counter, ids_1.qsize()))
+            print("Smjer semafora na mostu je ", self.my_direction)
+
+            # Čitaj zahtjeve za prelaz preko mosta dok ne istekne vrijeme ili su skupljena 3 zahtjeva
             while True:
-                if (abs(time.perf_counter() - start_time) >= 2 and self.q.qsize() == 0) or \
-                        (dir_1_counter >= 3 and self.my_direction == 1) or \
-                        (dir_0_counter >= 3 and self.my_direction == 0):
-                    print("Izlazim van uz")
-                    print("Velicine | q={}, c0={}, {}, c1={}, {}".format(self.q.qsize(), dir_0_counter, ids_0.qsize(),
-                                                                         dir_1_counter, ids_1.qsize()))
+                if (dir_1_counter >= 3 and self.my_direction == 1) or (dir_0_counter >= 3 and self.my_direction == 0) \
+                        or time.perf_counter() - start_time >= random.uniform(0.5, 1):
                     break
-                message, reg_plate, direction = self.q.get()
-                if message == "Ja bih na most" and direction == 0:
+                if self.q1.empty():
+                    continue
+                message, reg_plate, direction = self.q1.get_nowait()
+                if message == ZELIM_NA_MOST and direction == 0:
                     dir_0_counter += 1
                     ids_0.put(reg_plate)
-                elif message == "Ja bih na most" and direction == 1:
+                elif message == ZELIM_NA_MOST and direction == 1:
                     dir_1_counter += 1
                     ids_1.put(reg_plate)
                 else:
-                    self.q.put([message, reg_plate, direction])
+                    self.q1.put([message, reg_plate, direction])
 
+            # Odredi kojim autima je potrebno dojaviti da idu na most
             if self.my_direction == 0:
                 cars_to_take = min(3, dir_0_counter)
                 dir_0_counter -= cars_to_take
@@ -152,23 +128,32 @@ class Bridge:
                 for i in range(cars_to_take):
                     ids.append(ids_1.get())
 
+            # Posalji signal da se popnu na most
             for id_i in ids:
-                self.q.put(["Popni se", id_i, self.my_direction])
-                time.sleep(0.05)
+                self.q2.put([ODI_NA_MOST, id_i, self.my_direction])
+                time.sleep(0.05)  # kratko spavanje jer su auti jedan iza drugoga pa ne mogu istodobno svi na most
 
-            time.sleep(3)
+            # Auti su na mostu 1-3 sekunde
+            time.sleep(random.uniform(1, 3))
 
+            # Javi autima da moraju sici s mosta
             for id_i in ids:
-                self.q.put(["Sidi dole", id_i, self.my_direction])
-                time.sleep(0.05)
+                self.q2.put([SIDI_S_MOSTA, id_i, self.my_direction])
+                time.sleep(0.05)  # kratko spavanje jer silaze s mosta jedan za drugim
 
+            # Dodatno spavanje samo radi preglednijeg ispisa
             time.sleep(1)
 
+            # Promijeni smjer semafora na mostu
             self.my_direction = (self.my_direction + 1) % 2
 
-            active_cars = np.count_nonzero(np.array([car.process.exitcode is None for car in self.cars]))
+            # Provjeri koliko ih je još aktivnih
+            active_cars = 0
+            for car in self.cars:
+                if car.process.exitcode is None:
+                    active_cars += 1
 
 
 if __name__ == '__main__':
-    bridge = Bridge(1, 5, 10, 3)
+    bridge = Bridge(1, 5, 60, 3)
     bridge.run()
